f66e515e6bc2e970a68d7583d423b0904c07389b
hyyu <hubert080116@gmail.com>
2019-08-15 16:59:04 +0800

    add schema and documentation

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # 3rd Party
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_swagger',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -142,3 +143,7 @@ REST_FRAMEWORK = {
 }
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SITE_ID = 1
+SWAGGER_SETTINGS = {
+    'LOGIN_URL': 'rest_framework:login',
+    'LOGOUT_URL': 'rest_framework:logout',
+}
